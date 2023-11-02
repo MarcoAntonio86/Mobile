e45736062343cf51eca14a1d9898934923623f97
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2023-11-02 20:01:49 -0300

    lista finalizada

--- ListaTarefa/app/src/main/java/devandroid/marcoantonio/listatarefa/Model/Listada.java
+++ ListaTarefa/app/src/main/java/devandroid/marcoantonio/listatarefa/Model/Listada.java
@@ -1,4 +1,22 @@
 package devandroid.marcoantonio.listatarefa.Model;
 
 public class Listada {
+    private String insiratarefa;
+
+        public Listada () {}
+
+    public String getInsiratarefa() {
+        return insiratarefa;
+    }
+
+    public void setInsiratarefa(String insiratarefa) {
+        this.insiratarefa = insiratarefa;
+    }
+
+    @Override
+    public String toString() {
+        return "Listada{" +
+                "insiratarefa='" + insiratarefa + '\'' +
+                '}';
+    }
 }
--- ListaTarefa/app/src/main/java/devandroid/marcoantonio/listatarefa/View/MainActivity.java
+++ ListaTarefa/app/src/main/java/devandroid/marcoantonio/listatarefa/View/MainActivity.java
@@ -3,14 +3,83 @@ package devandroid.marcoantonio.listatarefa.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import devandroid.marcoantonio.listatarefa.Model.Listada;
 import devandroid.marcoantonio.listatarefa.R;
 
 public class MainActivity extends AppCompatActivity {
+        Listada vlrinserido;
+        Listada outrovlrinserido;
+
+        EditText idinsira;
+
+        Button btnverificar;
+        Button btncadastrar;
+        Button btnmarcar;
+        Button btnexcluir;
+        Button btnlistadas;
+        Button btnlimpar;
+        Button btnsalvar;
+        Button btnfinalizar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vlrinserido = new Listada();
+        vlrinserido.setInsiratarefa("Lavar");
+
+        outrovlrinserido = new Listada();
+
+        idinsira = findViewById(R.id.idinsira);
+
+        btnverificar = findViewById(R.id.btnverificar);
+        btncadastrar = findViewById(R.id.btncadastrar);
+        btnmarcar = findViewById(R.id.btnmarcar);
+        btnexcluir = findViewById(R.id.btnexcluir);
+        btnlistadas = findViewById(R.id.btnlistadas);
+        btnlimpar = findViewById(R.id.btnlimpar);
+        btnsalvar = findViewById(R.id.btnsalvar);
+        btnfinalizar = findViewById(R.id.btnfinalizar);
+
+        btnlimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                idinsira.setText("");
+            }
+        });
+
+        btnsalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outrovlrinserido.setInsiratarefa(idinsira.getText().toString());
+                Toast.makeText(MainActivity.this, "Dados Salvos" + outrovlrinserido.toString(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        btnfinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText( MainActivity.this, "Aplicativo Finalizado", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+
+
+
+
+
+
+
+
+
     }
 }
